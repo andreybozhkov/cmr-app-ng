c0eec06894e07c1bf5bf1cc11679afa227735708
andreybozhkov <bozhkov.andrey@gmail.com>
2019-04-11 16:59:21 +0300

    Added navbar 1st part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cmr-app-ng';
+  title = 'CMR App Angular';
 }
---
+++ src/app/config/Config.js
@@ -0,0 +1,4 @@
+export default {
+    kinveyAppKey: 'kid_S1XCFhYLV',
+    kinveyAppSecret: 'cdf2c68841d6429c87e582a967bbb04e'
+}
