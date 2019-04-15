b98a478f643b459eddbcc52331c167f85b99f4b3
andreybozhkov <bozhkov.andrey@gmail.com>
2019-04-15 17:02:15 +0300

    Added signup form 1st steps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/common/navbar/navbar.component';
+import { SignupComponent } from './components/user/signup/signup.component';
+
+const appRoutes: Routes = [
+  { path: 'signup', component: SignupComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes, { enableTracing: true }
+    ),
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
